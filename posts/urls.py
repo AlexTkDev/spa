66e382c6e06171d76d6c4ef3a5829c1f48fb3954
alex <a8696tk@gmail.com>
2025-02-11 16:29:16 +0200

    Add comment listing feature with pagination and sorting options

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, CommentCreateView
+from .views import PostListView, PostDetailView, PostCreateView, CommentCreateView, CommentListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="post_list"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post_detail"),
     path('post/create/', PostCreateView.as_view(), name="post_create"),
     path('post/<int:post_id>/comment/', CommentCreateView.as_view(), name="comment_create"),
-
+    path('comments/', CommentListView.as_view(), name='comment_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,23 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
 from posts.models import Post, Comment
 from posts.forms import PostForm, CommentForm
+from django.core.paginator import Paginator
+
+
+class CommentListView(ListView):
+    model = Comment
+    template_name = 'posts/comment_list.html'
+    context_object_name = 'comments'
+    paginate_by = 25
+
+    def get_queryset(self):
+        queryset = Comment.objects.all().order_by('-created_at')  # Сортировка по дате
+        return queryset
+
+    def get_ordering(self):
+        order_by = self.request.GET.get('order_by', '-created_at')  # Параметр для сортировки
+        return order_by
+
 
 class PostListView(ListView):
     model = Post
@@ -12,9 +29,25 @@ class PostListView(ListView):
     context_object_name = "posts"
 
     def get_queryset(self):
-        """sorting posts by created_at in descending order"""
-        return Post.objects.all().order_by('-created_at')
+        # Получаем все посты и применяем сортировку
+        order_by = self.request.GET.get('order_by', '-created_at')
+        return Post.objects.all().order_by(order_by)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Получаем все посты
+        posts_list = self.get_queryset()
+
+        # Пагинация
+        paginator = Paginator(posts_list, 25)  # 25 на странице
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        # Добавляем в контекст посты и дополнительные данные
+        context['posts'] = page_obj
+        context['order_by'] = self.request.GET.get('order_by', '-created_at')
+        return context
 
 class PostDetailView(DetailView):
     model = Post
